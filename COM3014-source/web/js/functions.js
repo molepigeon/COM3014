fbca2aa8b80377870a890f8ec71454e62f70fa3d
Jade Beesley <github@jademeteor.com>
2014-05-08 22:39:51 +0100

    Fixed AJAX error
    Meta tag state was being pulled incorrectly. Fixed.

--- COM3014-source/web/js/functions.js
+++ COM3014-source/web/js/functions.js
@@ -18,8 +18,12 @@ function signinCallback(authResult) {
       if (authResult['status']['signed_in']) {
         document.getElementById('signinButton').setAttribute('style', 'display: none');
         document.getElementById('signoutButton').setAttribute('style', 'display: inline-block');
-        idPass(authResult['code'],authResult['id_token'],authResult['access_token'],$("meta[property='google-state']").attr('content'));
-        console.log('User Token: ' + authResult['id_token']);
+        idPass(authResult['code'],authResult['id_token'],authResult['access_token'],$('meta[name=google-state]').attr('content'));
+       
+        console.log("User Code: "+authResult['code']);
+        console.log("ID Token: "+authResult['id_token']);
+        console.log("Access Token "+authResult['access_token']);
+        console.log("State "+$('meta[name=google-state]').attr('content'));
         
         
       } else {
@@ -39,7 +43,7 @@ function idPass(code,id,access,state) {
         state: state
     },
     type: "POST",
-    dataType : "text",
+    dataType : "text",    
     success: function( json ) {
         $( "#username" ).text( json.title ).text(json);
     },
